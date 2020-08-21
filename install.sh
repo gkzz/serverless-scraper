f468625c977180d0eca24add3fffe47804fa13be
gkzz <gakuji.tamaki@gmail.com>
2020-08-22 02:29:11 +0900

    update version of chrome/driver

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-CHROMEFILE=https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-41/stable-headless-chromium-amazonlinux-2017-03.zip
-CHROMEDRIVER=https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+#CHROMEFILE=https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-41/stable-headless-chromium-amazonlinux-2017-03.zip
+#CHROMEDRIVER=https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+
+CHROMEFILE=https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-55/stable-headless-chromium-amazonlinux-2017-03.zip
+CHROMEDRIVER=https://chromedriver.storage.googleapis.com/2.40/chromedriver_linux64.zip
 
 
 curl -SL $CHROMEFILE > headless-chromium.zip
