2f8457eccf3e0742c0311c9229e7ba4ae6c8f749
gkzz <gakuji.tamaki@gmail.com>
2020-08-29 21:04:56 +0900

    add lambda/.fonts

--- install.sh
+++ install.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-CHROMEVERSION="v.1.0.0-45"
-DRIVERVERSION="2.40"
+#CHROMEVERSION="v1.0.0-45"
+#DRIVERVERSION="2.40"
+
+CHROMEVERSION="v1.0.0-50"
+DRIVERVERSION="2.41"
 
 CHROMEFILE=https://github.com/adieuadieu/serverless-chrome/releases/download/${CHROMEVERSION}/stable-headless-chromium-amazonlinux-2017-03.zip
 CHROMEDRIVER=https://chromedriver.storage.googleapis.com/${DRIVERVERSION}/chromedriver_linux64.zip
-
-#CHROMEFILE=https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-41/stable-headless-chromium-amazonlinux-2017-03.zip
-#CHROMEDRIVER=https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
-
-
 
 curl -SL $CHROMEFILE > headless-chromium.zip
 unzip headless-chromium.zip 
@@ -20,4 +18,5 @@ rm headless-chromium.zip
 curl -SL $CHROMEDRIVER > chromedriver.zip
 unzip chromedriver.zip 
 rm -rf chromedriver.zip
+
 mv chromedriver headless-chromium selenium-layer/driver/
